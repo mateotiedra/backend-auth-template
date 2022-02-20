94fa1334bd3cb423971903e61a7686bdc841c08e
mateotiedra <mateo@tiedra.com>
2022-02-20 22:08:37 +0100

    correct async signup

--- app/controllers/auth.controller.js
+++ app/controllers/auth.controller.js
@@ -10,31 +10,35 @@ const User = db.user;
 const Op = db.Sequelize.Op;
 
 exports.signUp = (req, res) => {
-  const confirmationToken = crypto.randomBytes(16).toString('hex');
-  User.create({
-    email: req.body.email,
-    password: bcrypt.hashSync(req.body.password, 8),
-    confirmationToken: confirmationToken,
-    confirmationTokenGeneratedAt: Date.now(),
-  })
-    .then((user) => {
-      res.status(200).send({
-        message: 'User registered successfully! Please check your email',
-      });
-      /* nodemailer.sendConfirmationEmail(
-        user.username,
-        user.email,
-        user.confirmationCode
-      ); */
-      console.log(user.confirmationToken);
-    })
-    .catch(unexpectedErrorCatch(res));
+  bcrypt.hash(req.body.password, 10, function (err, hash) {
+    const password = hash;
+    console.log('hello');
+    crypto.randomBytes(16, function (err, buf) {
+      const confirmationToken = buf.toString('hex');
+      User.create({
+        email: req.body.email,
+        password: password,
+        confirmationToken: confirmationToken,
+        confirmationTokenGeneratedAt: Date.now(),
+      })
+        .then((user) => {
+          res.status(200).send({
+            message: 'User registered successfully! Please check your email',
+          });
+          // TODO : send the confirmation email here
+          console.log(user.confirmationToken);
+        })
+        .catch(unexpectedErrorCatch(res));
+    });
+  });
 };
 
 exports.confirmSignUp = (req, res) => {
-  req.user.status = 'active';
-  req.user.confirmationTokenGeneratedAt = 0;
-  req.user
+  const user = req.user;
+  user.status = 'active';
+  user.confirmationTokenGeneratedAt = 0;
+  user.confirmationToken = '';
+  user
     .save()
     .then(() => {
       res.status(200).send({ message: 'Mail confirmed!' });
