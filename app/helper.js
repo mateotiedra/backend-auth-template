53a2950b1fdbd02f39680f6ab880cbadbc9e5269
mateotiedra <mateo@tiedra.com>
2022-02-23 13:40:53 +0100

    change export practice for helper

--- app/helper.js
+++ app/helper.js
@@ -1,7 +1,12 @@
-exports.unexpectedErrorCatch = (res) => (err) => {
+const unexpectedErrorCatch = (res) => (err) => {
   res.status(500).send({ message: 'Unexpected error : ' + err.message });
 };
 
-exports.userNotFoundRes = (res, object = 'User') => {
+const userNotFoundRes = (res, object = 'User') => {
   return res.status(404).send({ message: object + ' not found.' });
+};
+
+module.exports = {
+  unexpectedErrorCatch,
+  userNotFoundRes,
 };
